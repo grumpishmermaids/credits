d00f86ff9e2a76b92ad63ac034f219cb107e571b
Keith Armstrong <keith1015@gmail.com>
2015-10-05 14:10:01 -0700

    fixed full path

--- index.sh
+++ index.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-pwd=`pwd`
+pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 if [ ! -e "$pwd/main" ]
 then
-  make
+  make -f $pwd/Makefile;
 fi
-echo "Creators of Grump\n"
-echo "Keith Armstrong"
-echo "Beasta Blaha"
-echo "Benjamin Davis"
-echo "Henry Hutcheson"
+echo "Creators of Grump\n";
+echo "Keith Armstrong";
+echo "Beasta Blaha";
+echo "Benjamin Davis";
+echo "Henry Hutcheson";
 while true
 do
-  $pwd/main -u keith.png & sleep .5 && $pwd/main -u beasta.png & sleep 1 && $pwd/main -u ben.png & sleep 1.5 && $pwd/main -u henry.png
+  $pwd/main -u keith.png & sleep .5 && $pwd/main -u beasta.png & sleep 1 && $pwd/main -u ben.png & sleep 1.5 && $pwd/main -u henry.png;
 done
