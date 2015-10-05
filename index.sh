0fde276e9129895db1bdf3bb1bb49c62e0663a3f
Keith Armstrong <keith1015@gmail.com>
2015-10-05 13:52:46 -0700

    Fixed path error

--- index.sh
+++ index.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ ! -e "main" ]
+pwd=`pwd`
+if [ ! -e "$pwd/main" ]
 then
   make
 fi
 while true
 do
-  ./main -u keith.png & sleep .5 && ./main -u ben.png & sleep 1 && ./main -u henry.png & sleep 1.5 && ./main -u beasta.png
+  $pwd/main -u keith.png & sleep .5 && $pwd/main -u ben.png & sleep 1 && $pwd/main -u henry.png & sleep 1.5 && $pwd/main -u beasta.png
 done
