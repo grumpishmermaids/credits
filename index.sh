4b026056a3c5978a6a4a2ab15c7d22125f5d8f71
Keith Armstrong <keith1015@gmail.com>
2015-10-05 14:01:23 -0700

    Updated credits paths and text

--- index.sh
+++ index.sh
@@ -4,7 +4,12 @@ if [ ! -e "$pwd/main" ]
 then
   make
 fi
+echo "Creators of Grump\n"
+echo "Keith Armstrong"
+echo "Beasta Blaha"
+echo "Benjamin Davis"
+echo "Henry Hutcheson"
 while true
 do
-  $pwd/main -u keith.png & sleep .5 && $pwd/main -u ben.png & sleep 1 && $pwd/main -u henry.png & sleep 1.5 && $pwd/main -u beasta.png
+  $pwd/main -u keith.png & sleep .5 && $pwd/main -u beasta.png & sleep 1 && $pwd/main -u ben.png & sleep 1.5 && $pwd/main -u henry.png
 done
